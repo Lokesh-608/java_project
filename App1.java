8891b054b8966cb38c491024d7928ade826314e6
Your Name <you@example.com>
2025-01-08 20:54:27 +0530

    init

---
+++ App1.java
@@ -0,0 +1,13 @@
+public class App1 {
+    public static void main(String[] args) {
+        int num[] = {7,8,9,3,2};
+        for (int i=0;i<5;i++){
+            System.out.println(num[i]);
+
+        }
+
+
+
+    }
+    
+}
